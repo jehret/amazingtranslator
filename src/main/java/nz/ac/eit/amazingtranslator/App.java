db7dae43d18932952b4e90aff4b5bb9471d67874
Jeanphi <jehret@jnesis.com>
2019-08-20 16:33:23 +1200

    Implemented Isuue #1 Cllecting the number to translate

--- src/main/java/nz/ac/eit/amazingtranslator/App.java
+++ src/main/java/nz/ac/eit/amazingtranslator/App.java
@@ -1,5 +1,7 @@
 package nz.ac.eit.amazingtranslator;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Please enter a number to translate" );
+        Scanner scan=new Scanner(System.in);
+        String numberAsString=scan.nextLine();
+        int numberToTranslate=Integer.parseInt(numberAsString);
+        System.out.println("Collected number" +numberToTranslate);
     }
 }
