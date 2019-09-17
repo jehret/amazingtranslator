0f7f76bfd32d8010bb5e08295cfbc734eaa13ae1
Jean-Philippe Ehret <jehret@jnesis.com>
2019-09-17 16:24:31 +1200

    Final version

--- src/main/java/nz/ac/eit/amazingtranslator/App.java
+++ src/main/java/nz/ac/eit/amazingtranslator/App.java
@@ -14,7 +14,7 @@ public class App
 
     private static void intializeTranslations(){
         frenchTranslations.put(1,"Un");
-        germanTranslations.put(1,"hjhj");
+        germanTranslations.put(1,"hjhjj");
     }
 
     public static void main( String[] args )
